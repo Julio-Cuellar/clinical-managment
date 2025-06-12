a69b2f299d9a47734c120abcc76650b5838ab681
Julio-Cuellar <anyaquimera1@gmail.com>
2025-06-11 18:14:35 -0600

    first commit

---
+++ patient-service/src/test/java/com/luminia/clinical/patient_service/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminia.clinical.patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
